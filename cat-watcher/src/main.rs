0ee940937c4b963d12a8abadb9df8d020d15729c
win11_Sora <vmwin11@mail.com>
2026-04-08 08:07:23 +0900

    feat Phase4 CLI引数

--- cat-watcher/src/main.rs
+++ cat-watcher/src/main.rs
@@ -1,19 +1,47 @@
 use crate::error::AppError;
-use std::path::Path;
-mod error;
+use clap::Parser;
+use std::path::PathBuf;
 mod config;
+mod error;
 mod placeholder;
 
-fn main() -> Result<(), AppError>{
-	let global_conf_path = "config/global.toml";
-	let global_conf = config::load_global_config(&Path::new(global_conf_path))?;
-	// println!("{:#?}", global_conf);
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    global: PathBuf,
+    #[arg(short, long)]
+    rules: PathBuf,
+    #[arg(long)]
+    dry_run: bool,
+    #[arg(long)]
+    log_level: Option<String>,
+    #[arg(long)]
+    validate: bool,
+}
 
-	let rules_conf_path = "config/rules.toml";
-	let rules_conf = config::load_rules_config(&Path::new(rules_conf_path))?;
-	// println!("{:#?}", rules_conf);
+fn main() -> Result<(), AppError> {
+    let cli = Cli::parse();
 
-	config::validate_global_config(&global_conf)?;
-	config::validate_rules_config(&rules_conf)?;
-	Ok(())	
+    let result = run(&cli);
+    match result {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            eprintln!("実行エラー: {}", e);
+            std::process::exit(1);
+        }
+    };
+}
+
+fn run(cli: &Cli) -> Result<(), AppError> {
+    let global_config = config::load_global_config(&cli.global)?;
+    let rules_conf = config::load_rules_config(&cli.rules)?;
+
+    config::validate_global_config(&global_config)?;
+    config::validate_rules_config(&rules_conf)?;
+
+    if cli.validate {
+        println!("バリデーション処理成功");
+        return Ok(());
+    }
+    Ok(())
 }
